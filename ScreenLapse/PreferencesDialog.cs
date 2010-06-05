6e645f58b8898f9d37133f31e3890f95e31a10df
Anirudh Sanjeev <andy@ninjagod.com>
2010-06-05 12:08:56 +0530

    Modified Preferences dialog

--- ScreenLapse/PreferencesDialog.cs
+++ ScreenLapse/PreferencesDialog.cs
@@ -6,10 +6,33 @@ namespace ScreenLapse
 
 	public partial class PreferencesDialog : Gtk.Dialog
 	{
-
+		
 		public PreferencesDialog ()
 		{
 			this.Build ();
+			
+			// Load the settings into the GUI
+			filechooserbutton2.CurrentFolder = Preferences.SavePath;
+			spinbutton1.Value = Preferences.Interval;
+			spinbutton2.Value = Preferences.ScalePercentage;
+			spinbutton3.Value = Preferences.PlaybackDelay;
+		}
+		protected virtual void ClosePref (object sender, System.EventArgs e)
+		{
+			this.Hide();
+			this.Dispose();
+		}
+		
+		protected virtual void SavePrefAndClose (object sender, System.EventArgs e)
+		{
+			Preferences.SavePath = filechooserbutton2.CurrentFolder;
+			Preferences.Interval = spinbutton1.Value;
+			Preferences.ScalePercentage = spinbutton2.Value;
+			Preferences.PlaybackDelay = spinbutton3.Value;
+			
+			Preferences.WriteToGConf();
+			this.Hide();
+			this.Dispose();
 		}
 	}
 }
